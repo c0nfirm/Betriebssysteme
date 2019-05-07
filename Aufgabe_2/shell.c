8fafce165babcdb1931237f311623b66b780557d
Julian Rathenow <julian_rathenow91@hotmaild.e>
2019-05-07 09:48:10 +0200

    Aufgabe 2 Shell
    vorlagedateien  der 2. Aufgabe (Shell)

---
+++ Aufgabe_2/shell.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#include "array.h"
+
+int main(void)
+{
+	return 0;
+}
